fadd024dec3b4792db97bdbce6d65c9b9e020f53
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-12 11:31:57 -0300

    fluxo das classe contato e agenda sendo criadas. com adicionar e remover contato em progresso.

--- agenda_de_contatos.py
+++ agenda_de_contatos.py
@@ -18,3 +18,26 @@ A lista de contato irá ter as funcionalidades de:
 4.buscar
 5.listar todos
 '''
+
+class Contato:
+    def __init__(self,nome,telefone):
+        self.nome = nome
+        self.telefone = telefone
+
+class Agenda:
+    def __init__(self):
+        self.lista_contatos = {}
+
+    def adicionar_contato(self,nome,telefone):
+        contato = Contato(nome,telefone)
+        self.lista_contatos[nome] = contato.telefone
+    
+    def remover_contato(self,nome,telefone):
+        if nome in self.lista_contatos:
+            self.lista_contatos.pop()
+
+agenda = Agenda()
+print(agenda.adicionar_contato("Maria","11988443300"))
+print(agenda.adicionar_contato("Julio","11985243400"))
+print(agenda.remover_contato("Julio","11985243400"))
+print(agenda.lista_contatos)
