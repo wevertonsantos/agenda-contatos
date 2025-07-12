72d71106a26e246ed707ecaf75c3537ebae9891d
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-12 11:10:22 -0300

    estrutura por trás do programa.

---
+++ agenda_de_contatos.py
@@ -0,0 +1,20 @@
+'''
+O que essa agenda irá ter?
+1.adicionar contato
+2.remover contato
+4.alterar contato
+3.listar contato
+4.buscar contato
+
+O que o contato terá?
+pessoa:
+    1.nome
+    2.telefone
+
+A lista de contato irá ter as funcionalidades de:
+1.remover
+2.adicionar
+3.alterar
+4.buscar
+5.listar todos
+'''
