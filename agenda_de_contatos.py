7f78f2d3d10bda3ea4c99a78a7efaa1a1f39ed3e
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-12 12:20:45 -0300

    not telefone.isdigit()

--- agenda_de_contatos.py
+++ agenda_de_contatos.py
@@ -30,8 +30,11 @@ class Agenda:
 
     def adicionar_contato(self,nome,telefone):
         if len(nome) > 0 and len(telefone) > 0:
-            contato = Contato(nome,telefone)
-            self.lista_contatos[nome] = contato.telefone
+            if not telefone.isdigit():
+                print("Número de telefone inválido!")
+            else:
+                contato = Contato(nome,telefone)
+                self.lista_contatos[nome] = contato.telefone
         else:
             print("Você digitou dados vazio. Tente novamente!")
     
@@ -70,18 +73,18 @@ def main():
             if escolha == 1:
                 agenda.listar_contatos()
             elif escolha == 2:
-                nome_novo_contato = input("Digite o nome do contato que gostaria: ")
+                nome_novo_contato = input("Digite o nome do contato que gostaria: ").strip().title()
                 numero_novo_contato = input("Digite o número do contato: ")
                 agenda.adicionar_contato(nome_novo_contato,numero_novo_contato)
             elif escolha == 3:
-                nome_contato_para_remover = input("Digite o nome do contato que deseja remover: ")
+                nome_contato_para_remover = input("Digite o nome do contato que deseja remover: ").strip().title()
                 agenda.remover_contato(nome_contato_para_remover)
             elif escolha == 4:
-                nome_contato_para_alterar = input("Digite o nome do contato que deseja alterar: ")
+                nome_contato_para_alterar = input("Digite o nome do contato que deseja alterar: ").strip().title()
                 numero_contato_para_alterar = input("Digite o novo número do contato: ")
                 agenda.alterar_contato(nome_contato_para_alterar,numero_contato_para_alterar)
             elif escolha == 5:
-                nome_contato_para_buscar = input("Digite o nome do contato para buscar: ")
+                nome_contato_para_buscar = input("Digite o nome do contato para buscar: ").strip().title()
                 agenda.buscar_contato(nome_contato_para_buscar)
             elif escolha == 6:
                 print("Você saiu da agenda!")
